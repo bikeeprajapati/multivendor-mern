cc0ef0fc42c5649ddf6e143f40f5618edffdbcdd
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-06-11 17:13:16 +0545

    feat: add Login and Signup pages with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import"./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LoginPage,SignupPage } from './Routes.js';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ frontend/src/Routes.js
@@ -0,0 +1,3 @@
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+export { LoginPage, SignupPage};
