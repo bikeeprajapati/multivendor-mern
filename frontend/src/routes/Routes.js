558e72dd14649b583eafdcb6e680746791e47732
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-06-29 12:49:59 +0545

    feat: add CheckoutPage and PaymentPage components

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,4 +1,4 @@
-import LoginPage from "../pages/Login.jsx";
+import LoginPage from "../pages/Login";
 import SignupPage from "../pages/SignupPage";
 import ActivationPage from "../pages/ActivationPage";
 import HomePage from "../pages/HomePage";
