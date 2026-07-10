090e00d3c3426b38ebdcb3670fe820c85bf7d69e
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-10 12:52:27 +0545

    fix: refactor sendMail function to use BrevoClient for sending emails

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,20 +1,19 @@
-const brevo = require("@getbrevo/brevo");
+const { BrevoClient } = require("@getbrevo/brevo");
 
-const apiInstance = new brevo.TransactionalEmailsApi();
-apiInstance.authentications.apiKey.apiKey = process.env.BREVO_API_KEY;
+const brevo = new BrevoClient({
+    apiKey: process.env.BREVO_API_KEY,
+});
 
 const sendMail = async (options) => {
-    const sendSmtpEmail = new brevo.SendSmtpEmail();
-
-    sendSmtpEmail.sender = {
-        name: "ShopO",
-        email: process.env.BREVO_SENDER_EMAIL,
-    };
-    sendSmtpEmail.to = [{ email: options.email }];
-    sendSmtpEmail.subject = options.subject;
-    sendSmtpEmail.textContent = options.message;
-
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    await brevo.transactionalEmails.sendTransacEmail({
+        subject: options.subject,
+        textContent: options.message,
+        sender: {
+            name: "ShopO",
+            email: process.env.BREVO_SENDER_EMAIL,
+        },
+        to: [{ email: options.email }],
+    });
 };
 
 module.exports = sendMail;
