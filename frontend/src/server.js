09e71b3490d669220e94209849771bea6541742f
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-10 11:14:33 +0545

    fix: update socket server endpoint from localhost to production URL

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000";
+export const server = "https://multivendor-socket-61tp.onrender.com";
+export const backend_url = "https://multivendor-backend-219u.onrender.com";
